c0ca6e97fb5cac04e903cca0c5c05ba61d8544c2
A-I-18 <antonio.ionta.18@gmail.com>
2025-07-11 09:43:26 +0200

    feat: implement request logic
    get content type by filename

--- js/request-logic.js
+++ js/request-logic.js
@@ -2,7 +2,25 @@
 // extension off of the supplied filename, but we can't figure out the rest of
 // the function to use it! We hope this is useful to you!
 
+const contentTypes = Object.freeze({
+  'HTML': 'text/html',
+  'CSS': 'text/css',
+  'JPEG': 'image/jpeg',
+  'TEXT': 'text/plain'
+});
 
 function getContentType(filename) {
   const extension = filename.match(/.*\.([^\.]*)$/)[1];
+
+  switch (extension) {
+    case 'html':
+      return contentTypes.HTML;
+    case 'css':
+      return contentTypes.CSS;
+    case 'jpeg':
+    case 'jpg':
+      return contentTypes.JPEG;
+    default:
+      return contentTypes.TEXT;
+  }
 }
